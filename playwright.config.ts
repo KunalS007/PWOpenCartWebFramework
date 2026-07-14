1c1f558b67d0a5990e7655985ff60e9798d847ea
Kunal Shah <kunals007@gmail.com>
2026-07-14 18:51:44 +0530

    E2e updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,26 +17,26 @@ export default defineConfig({
   workers: process.env.CI ? '50%' : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
 
-  // reporter: [
-  //   ["list"],
-  //   ["html",{outputFolder:"reports/html-report",open:"never"}],
-  //   ["allure-playwright",{
-  //     outputFolder:"allure-results",
-  //     suiteTitle:true,
-  //   }],
+  reporter: [
+    ["list"],
+    ["html",{outputFolder:"reports/html-report",open:"never"}],
+    ["allure-playwright",{
+      outputFolder:"allure-results",
+      suiteTitle:true,
+    }],
 
-   reporter: process.env.CI
-    ? [
-      ["blob"],
-      ["html", { outputFolder: "reports/html-report", open: "never" }],
-      ["allure-playwright", { outputFolder: "allure-results", suiteTitle: true }],
-    ]
-    : [
-      ["blob", { outputDir: "blob-report" }],
-      ["list"],
-      ["html", { outputFolder: "reports/html-report", open: "never" }],
-      ["allure-playwright", { outputFolder: "allure-results", suiteTitle: true }],
-    ],
+  //  reporter: process.env.CI
+  //   ? [
+  //     ["blob"],
+  //     ["html", { outputFolder: "reports/html-report", open: "never" }],
+  //     ["allure-playwright", { outputFolder: "allure-results", suiteTitle: true }],
+  //   ]
+  //   : [
+  //     ["blob", { outputDir: "blob-report" }],
+  //     ["list"],
+  //     ["html", { outputFolder: "reports/html-report", open: "never" }],
+  //     ["allure-playwright", { outputFolder: "allure-results", suiteTitle: true }],
+  //   ],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
